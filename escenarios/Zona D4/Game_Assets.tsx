d184301df12ee1527ec574f101a70ba75a032195
Luis Mateo <33287751+LuisIgnacio98@users.noreply.github.com>
2022-02-02 21:08:06 -0400

    ZONA D4

---
+++ escenarios/Zona D4/Game_Assets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Game_Assets" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="D:/Descargas/Game_Assets.png" width="264" height="176"/>
+</tileset>
---
+++ escenarios/Zona D4/wooden_stairs-ns.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="wooden_stairs-ns" tilewidth="32" tileheight="32" tilecount="105" columns="7">
+ <image source="../wooden_stairs-ns.png" width="224" height="480"/>
+</tileset>
