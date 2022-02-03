7de87a819ea6f071a8774fd666b802f2d3ade332
rquiroz0712 <r.quiroz@actbold.com>
2022-02-03 11:41:51 -0400

    Agregando zona A1

---
+++ escenarios/Zona A1/Assets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Assets" tilewidth="16" tileheight="16" tilecount="180" columns="9">
+ <image source="../../assets/legacy_fantasy_castle_prision/Assets/Assets.png" width="144" height="320"/>
+</tileset>
